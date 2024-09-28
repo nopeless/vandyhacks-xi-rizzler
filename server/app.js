e56bf1efe6ece009dd6d7d9abf332f0d07164558
nopeless <38830903+nopeless@users.noreply.github.com>
2024-09-28 18:15:57 -0500

    use new cookie parser

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+
 import api from "./api-route.js";
 
 const app = express();
 
 // for authentication/authorization
-app.use(express.cookieParser());
+app.use(cookieParser());
 // json request parsing
 app.use(express.json());
 // for forms
