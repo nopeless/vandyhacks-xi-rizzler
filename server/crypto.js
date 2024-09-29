40023bf109684cbd621789ddb869d2deeb6c61ea
nopeless <38830903+nopeless@users.noreply.github.com>
2024-09-29 04:37:05 -0500

    add argument validation

--- server/crypto.js
+++ server/crypto.js
@@ -2,11 +2,13 @@ import bcrypt from "bcrypt";
 import crypto from "node:crypto";
 
 export async function hashPassword(password) {
+  if (!password) throw new Error("Password missing");
   const rounds = 10;
   return await bcrypt.hash(password, rounds);
 }
 
 export async function validatePassword(password, hash) {
+  if (!password || !hash) throw new Error("Invalid argument");
   return await bcrypt.compare(password, hash);
 }
 
